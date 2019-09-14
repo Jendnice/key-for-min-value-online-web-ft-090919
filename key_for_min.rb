6bcfe8ff6f9168dd631cd025b86e5e23a02d4217
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-14 14:34:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil 
-  lowest_value = nil
+  lowest_value = 0
   
     name_hash.each do |key, value|
       if lowest_value == nil || value < lowest_value 
