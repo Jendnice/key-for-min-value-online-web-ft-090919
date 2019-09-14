24ff66819300bf845d5ec79b15ceba758b6c95cf
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-14 13:53:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,15 @@
 def key_for_min_value(name_hash)
   answer_key = []
   
-  name_hash.collect do |key, value|
-
- return key 
+   if name_hash = {}
+   return nil 
+   else 
+    name_hash.collect do |key, value|
+    key[value_a] <=> key[value_b]  
+    answer_key << name_hash.first 
+  end 
+ end 
+ answer_key[1] 
 end
 
- if name_hash = {}
-   return nil 
- end 
+ 
